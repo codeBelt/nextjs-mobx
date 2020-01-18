cbbd97f9fe338e4b03cda665c2fe7774a6596231
Robert S <rsavian@nerdery.com>
2020-01-18 09:10:18 -0600

    move webpack

--- next.config.js
+++ next.config.js
@@ -8,27 +8,30 @@ const withStyles = require('@webdeb/next-styles');
 //   targetDirectory : 'static/'
 // });
 
+// exportPathMap = () => {
+//     return {
+//         '/': { page: '/' },
+//     }
+// }
+
+const webpack = (config, options) => {
+  config.resolve.alias['environment'] = path.join(__dirname, 'src', 'environments', process.env.CLIENT_ENV);
+
+  config.module.rules.push({
+    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+    use: {
+      loader: 'url-loader',
+      options: {
+        limit: 100000
+      }
+    }
+  });
+
+  return config;
+};
+
 module.exports = withStyles({
   sass: true, // use .scss files
   modules: true,
-  webpack: function(config, options) {
-    config.resolve.alias['environment'] = path.join(__dirname, 'src', 'environments', process.env.CLIENT_ENV);
-
-    config.module.rules.push({
-      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-      use: {
-        loader: 'url-loader',
-        options: {
-          limit: 100000
-        }
-      }
-    })
-
-    return config;
-  },
-  // exportPathMap: function () {
-  //     return {
-  //         '/': { page: '/' },
-  //     }
-  // }
+  webpack,
 });

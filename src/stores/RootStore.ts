686be547e501ac60cbef3db0ff91a08557da8eaf
Robert S <rsavian@nerdery.com>
2020-01-18 18:59:00 -0600

    add SingletonRouter to root store

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,9 +1,10 @@
 import { ShowsStore } from './shows/ShowsStore';
 import { ToastsStore } from './toasts/ToastsStore';
-import {Router} from 'next/router';
+import Router from 'next/router'
+import {SingletonRouter} from 'next/dist/client/router';
 
 export default class RootStore {
-  // readonly router: Router = Router;
+  readonly router: SingletonRouter = Router;
   readonly showsStore: ReturnType<typeof ShowsStore>;
   readonly toastsStore: ReturnType<typeof ToastsStore>;
 

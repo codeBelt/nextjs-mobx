c701cb6487e5e4d6231af9ee2a27599006fdf6c4
Robert S <rsavian@nerdery.com>
2020-01-18 18:51:02 -0600

    add @types/react-dom and remove classnames

--- src/components/shared/loading-indicator/LoadingIndicator.tsx
+++ src/components/shared/loading-indicator/LoadingIndicator.tsx
@@ -1,16 +1,14 @@
 import styles from './LoadingIndicator.module.scss';
 
-import React, { useMemo } from 'react';
-import classNames from 'classnames';
+import React  from 'react';
 import { Loader } from 'semantic-ui-react';
 
 interface IProps {
   readonly isActive?: boolean;
-  readonly className?: string;
 }
 export const LoadingIndicator: React.FC<IProps> = (props) => {
-  const { isActive = false, className = undefined, children } = props;
-  const cssClasses = useMemo(() => classNames(className, { [styles.wrapper]: isActive }), [className, isActive]);
+  const { isActive = false, children } = props;
+  const cssClasses = isActive ? styles.wrapper : undefined;
 
   return (
     <div className={cssClasses}>

3ad07a5b1c081678d90b49411d50d03ecb23f917
Robert S <rsavian@nerdery.com>
2020-01-25 12:11:30 -0600

    add getInitialProps to EpisodesPage.tsx

--- src/components/episodes-page/EpisodesPage.tsx
+++ src/components/episodes-page/EpisodesPage.tsx
@@ -1,22 +1,18 @@
 // import styles from './EpisodesPage.module.scss';
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import IEpisodeTable from '../../stores/shows/computed/IEpisodeTable';
 import { LoadingIndicator } from '../shared/loading-indicator/LoadingIndicator';
 import { EpisodesTable } from './components/episodes-table/EpisodesTable';
 import { observer } from 'mobx-react';
-import { rootStore } from '../App';
 import { PageLayout } from '../shared/PageLayout';
+import { createRootStore, useMobxStores } from '../../stores/stores';
+import { NextPage, NextPageContext } from 'next';
 
 interface IProps {}
 
-export const EpisodesPage: React.FC<IProps> = observer((props) => {
-  const { showsStore } = rootStore;
-
-  useEffect(() => {
-    showsStore.requestEpisodes();
-  }, [showsStore]);
-
+export const EpisodesPage: NextPage<IProps> = observer((props) => {
+  const { showsStore } = useMobxStores();
   const { isRequesting } = showsStore.episodes;
   const episodeTables = showsStore.selectEpisodes;
 
@@ -29,3 +25,11 @@ export const EpisodesPage: React.FC<IProps> = observer((props) => {
     </PageLayout>
   );
 });
+
+EpisodesPage.getInitialProps = async (content: NextPageContext) => {
+  const data = await createRootStore().showsStore.requestEpisodes();
+
+  return {
+    episodes: data,
+  };
+};

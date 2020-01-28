0d46d336ced7cbcc798a9c3be10ee9e153d424eb
Robert S <rsavian@nerdery.com>
2020-01-27 21:41:46 -0600

    add storybook scss

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,6 @@
-module.exports = ({ config }) => {
+const path = require('path');
+
+module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
@@ -7,7 +9,34 @@ module.exports = ({ config }) => {
     },
   });
 
+  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+  // You can change the configuration based on that.
+  // 'PRODUCTION' is used when building the static version of storybook.
+  config.module.rules.push({
+    test: /\.scss$/,
+    loaders: [
+      require.resolve('style-loader'),
+      {
+        loader: require.resolve('css-loader'),
+        options: {
+          importLoaders: 1,
+          modules: {
+            mode: 'local',
+            localIdentName: '[path][name]__[local]--[hash:base64:5]',
+            // localIdentName: '[sha1:hash:hex:4]',
+            context: path.resolve(__dirname, 'src'),
+            hashPrefix: 'my-custom-hash',
+          },
+        },
+      },
+      require.resolve('sass-loader')
+    ],
+  });
+
+
   config.resolve.extensions.push('.ts', '.tsx');
 
   return config;
 };
+
+// https://gist.github.com/justincy/b8805ae2b333ac98d5a3bd9f431e8f70

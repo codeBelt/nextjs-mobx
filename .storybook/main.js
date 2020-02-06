5922e39aaad77b965c78e8b0bd3a8cf72ce41504
Robert S <rsavian@nerdery.com>
2020-02-06 11:18:55 -0600

    add docs

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,12 +2,13 @@
 
 module.exports = {
   stories: ['../src/**/*.stories.(tsx|mdx)'],
-  addons: [
-    '@storybook/addon-actions/register',
-    '@storybook/addon-links/register',
-  ]
+  addons: ['@storybook/addon-actions/register', '@storybook/addon-links/register'],
 };
 
 // https://github.com/storybookjs/storybook/tree/master/addons/knobs
 // https://github.com/storybookjs/storybook/tree/master/addons/storyshots
 // https://github.com/storybookjs/storybook/tree/master/addons/viewport
+
+// https://dev.to/bdougieyo/automating-my-storybook-deployment-n8d
+
+// https://storybook.js.org/docs/configurations/standalone-options/
